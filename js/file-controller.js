d34dac6141f589668b837bd92f667e96bd5138b2
hebert benigno <hebertramos09@gmail.com>
2023-04-27 11:31:50 -0300

    creating functionality to the elements of the pages to make it more usualy to the client

---
+++ js/file-controller.js
@@ -0,0 +1,16 @@
+
+
+
+//código que me permite dar um console.log() nos dados do arquivo
+
+// const file = this.files[0];
+// const reader = new FileReader();
+
+// if(file){
+//     reader.readAsText(file);
+// }
+
+// reader.addEventListener('load', function(){
+//     console.log(reader.result.split(','));
+// });
+
