5688368ba53bcb8b11494c1feff889ad5a42170c
hebert benigno <hebertramos09@gmail.com>
2023-05-10 16:33:05 -0300

    creating a function to read the file and get a array with objects conting the file data

---
+++ js/elements.js
@@ -0,0 +1,24 @@
+import * as FileController from './file-controller.js';
+
+// MAIN PAGE
+    // Card input file
+export const cardEnterFileMainPage = document.querySelector('#entrance-card-enter-file');
+export const fileNameMainPage = document.querySelector('#file-name');
+export const inputFileMainPage = document.querySelector('#file-input');
+    //Card general informations
+export const cardShowAllInformationsMainPage = document.querySelector("#entrance-card-general-informations")
+    //Card interval informations
+export const cardShowInformationPerIntervalMainPage = document.querySelector("#entrance-card-information-time-interval");
+
+    //Formulare
+export const cardFormIntervalMainPage = document.querySelector("#span-card-form");
+export const inputStartingDistanceMainPage = document.querySelector('#starting-distance');
+export const inputFinalDistanceMainPage = document.querySelector('#final-distance');
+export const btnConfirmMainPage = document.querySelector('#btn-confirm');
+
+
+//General informations page
+
+export const fastActiviteGeneralInformationsPage = document.querySelector('#paragraph-fastes-activity');
+
+//Interval information Page
--- js/file-controller.js
+++ js/file-controller.js
@@ -1,16 +1,56 @@
+import * as elements from './elements.js';
 
+var fileData = [];
 
+export async function getDataFromFile(filePath){
 
-//código que me permite dar um console.log() nos dados do arquivo
+    const file = filePath.files[0];
+    const reader = new FileReader();
 
-// const file = this.files[0];
-// const reader = new FileReader();
+    const fileReadPromise = new Promise((resolve, error) => {
+        reader.addEventListener('load', () => {
+            resolve(reader.result);
+        });
+        reader.addEventListener('error', () => {
+            error(reader.error);
+        })
+    });
 
-// if(file){
-//     reader.readAsText(file);
+    reader.readAsText(file);  
+
+    const fileArray = await fileReadPromise;
+
+    const lines = fileArray.trim().split('\n');
+    const header = lines[0].trim().split(',');
+    const data = [];
+
+    for(let i = 1; i < lines.length; i++){
+        const values = lines[i].split(',');
+        const obj = {};
+
+        for(let j = 0; j < header.length; j++){
+            const value = values[j].trim();
+            obj[header[j]] = isNaN(value) ? value : parseFloat(value);
+        }
+
+        data.push(obj);
+    }
+
+    fileData = data;
+ 
+}
+
+export async function gettingArrayData(filePath){
+    await getDataFromFile(filePath);
+
+    return fileData;
+}
+
+//Informação da velocidade média do primeiro objeto do array
+
+// export async function getSpeedObjectOne(){
+//     const objectOne = fileData[0];
+//     const objectOneAvarageSpeed = objectOne['Average Speed (km/h)'];
+
+//     return objectOneAvarageSpeed;
 // }
-
-// reader.addEventListener('load', function(){
-//     console.log(reader.result.split(','));
-// });
-
